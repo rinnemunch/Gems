994ee2c63db646b89bb9728d5ca0ffdd40d06fc2
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-10 18:45:07 -0400

    Instructions panel start

--- Assets/Scripts/Game Managers/MainMenuManager.cs
+++ Assets/Scripts/Game Managers/MainMenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject instructionsPanel;
     public void StartGame()
     {
         SceneManager.LoadScene("Level1");
@@ -12,5 +13,13 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
         Debug.Log("Game Quit"); 
     }
+    public void OpenInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
 
+    public void CloseInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
 }
