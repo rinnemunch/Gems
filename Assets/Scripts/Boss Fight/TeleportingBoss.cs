410c74523716088bb5027d2c5120e46aaa6c5175
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-18 20:35:49 -0400

    Fade in out for boss teleport

--- Assets/Scripts/Boss Fight/TeleportingBoss.cs
+++ Assets/Scripts/Boss Fight/TeleportingBoss.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TeleportingBoss : MonoBehaviour
@@ -5,16 +6,27 @@ public class TeleportingBoss : MonoBehaviour
     public float teleportInterval = 2f;
     public Vector2 minBounds = new Vector2(-8f, -3f);
     public Vector2 maxBounds = new Vector2(8f, 3f);
+    public SpriteRenderer sr;
 
     void Start()
     {
-        InvokeRepeating(nameof(Teleport), 0f, teleportInterval);
+        InvokeRepeating(nameof(StartTeleport), 0f, teleportInterval);
     }
 
-    void Teleport()
+    void StartTeleport()
     {
+        StartCoroutine(TeleportEffect());
+    }
+
+    IEnumerator TeleportEffect()
+    {
+        sr.color = new Color(1, 1, 1, 0); // Fade out
+        yield return new WaitForSeconds(0.2f);
+
         float x = Random.Range(minBounds.x, maxBounds.x);
         float y = Random.Range(minBounds.y, maxBounds.y);
         transform.position = new Vector3(x, y, 0f);
+
+        sr.color = new Color(1, 1, 1, 1); // Fade in
     }
 }
